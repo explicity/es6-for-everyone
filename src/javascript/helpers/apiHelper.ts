b392341b199a2f0d0a691930a9ff2c8f7bd62cb6
Olya Yakymovych <msolya@outlook.com>
2019-07-11 17:20:46 +0300

    rewrite apiHelper

--- src/javascript/helpers/apiHelper.ts
+++ src/javascript/helpers/apiHelper.ts
@@ -1,19 +1,23 @@
-const API_URL =
+const API_URL: string =
   "https://api.github.com/repos/binary-studio-academy/stage-2-es6-for-everyone/contents/resources/api/";
 
-function callApi(endpoind, method) {
-  const url = API_URL + endpoind;
-  const options = {
+interface IOptions {
+  method: string
+}
+
+async function callApi(endpoind: string, method: string) {
+  const url: string = API_URL + endpoind;
+  const options: IOptions = {
     method
   };
 
-  return fetch(url, options)
-    .then(response =>
-      response.ok ? response.json() : Promise.reject(Error("Failed to load"))
-    )
-    .catch(error => {
-      throw error;
-    });
+  try {
+    const response = await fetch(url, options);
+    return await (response.ok ? response.json() : Promise.reject(Error("Failed to load")));
+  }
+  catch (error) {
+    throw error;
+  }
 }
 
 export { callApi };
