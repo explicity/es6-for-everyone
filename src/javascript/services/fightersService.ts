b1973ad0cba08144e2091ea7954b2c84bb9198a2
Olya Yakymovych <msolya@outlook.com>
2019-07-11 17:36:13 +0300

    rewrite fighterService

--- src/javascript/services/fightersService.ts
+++ src/javascript/services/fightersService.ts
@@ -1,19 +1,19 @@
-import { callApi } from "../helpers/apiHelper.ts";
+import { callApi } from "../helpers/apiHelper";
 
 class FighterService {
-  async getFighters() {
-    const endpoint = "fighters.json";
+  public async getFighters(): Promise<boolean> {
+    const endpoint: string = "fighters.json";
 
     return await this.getCallApi(endpoint);
   }
 
-  async getFighterDetails(_id) {
-    const endpoint = `details/fighter/${_id}.json`;
+  public async getFighterDetails(_id: string): Promise<boolean> {
+    const endpoint: string = `details/fighter/${_id}.json`;
 
     return await this.getCallApi(endpoint);
   }
 
-  async getCallApi(endpoint) {
+  private async getCallApi(endpoint: string): Promise<boolean> {
     try {
       const apiResult = await callApi(endpoint, "GET");
 
